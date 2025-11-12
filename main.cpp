8a16072cb98443651a311d37b3caa6e9cdc1e542
AbdulrahmanMohamedSanaa <157839575+AbdulrahmanMohamedSanaa@users.noreply.github.com>
2025-11-12 15:16:04 +0200

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,3 +1,5 @@
+#include <iostream>
+using namespace std;
 #include "Animal.h"
 #include "Bird.h"
 #include "Mammal.h"
@@ -8,9 +10,28 @@
 using namespace std;
 
 
-// ============== MAIN FUNCTION ==============
-int main() {
+
+ Animal::Animal(string name, int age, bool isHungry) : name(name),age(age),isHungry(isHungry) {};
+ Animal::Animal(string name, int age, bool isHungry) : name(""),age(0),isHungry(false) {};
+ Reptile::Reptile(string name, int age, bool isHungry,bool isVenomous): Animal(), isVenomous(false) {};
+ Reptile::Reptile(string name, int age, bool isHungry,bool isVenomous): Animal(name,age,isHungry), isVenomous(isVenomous) {};
+  mammal::mammal(string name, int age, bool isHungry,string fur): Animal(), isVenomous(false) {};
+ mammal::mammal(string name, int age, bool isHungry,string fur): Animal(name,age,isHungry), fur("fur") {};
+  Bird::Bird(string name, int age, bool isHungry,float wingspan): Animal(), isVenomous(false) {};
+ Bird::Bird(string name, int age, bool isHungry,float wingspan): Animal(name,age,isHungry), isVenomous(isVenomous) {};
+enclosure::enclosure(int capacity, int currentCount) : capacity(0), currentCount(0) {};
+enclosure::enclosure(int capacity, int currentCount) : capacity(capacity), currentCount(currentCount) {};
+  Visitor::Visitor(string name, int ticketBrought): name(""), ticketBrought(0) {};
+ Visitor::Visitor(string name, int ticketBrought): name(name), ticketBrought(ticketBrought) {};
+
+voidAnimal::feed(){
+    cout << "feed"
+}
+
+
+int main()
+{
     
-    
+
     return 0;
 }
