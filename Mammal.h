d19462171f3c64d38b2763bc3e3c1163f09199ad
AbdulrahmanMohamedSanaa <157839575+AbdulrahmanMohamedSanaa@users.noreply.github.com>
2025-11-12 15:14:28 +0200

    Update Mammal.h

--- Mammal.h
+++ Mammal.h
@@ -2,12 +2,12 @@
 #define MAMMAL_H
 
 #include "Animal.h"
-
-//define mammal class here
-
-
-
-
-
-
-#endif
+ class mammal: public Animal{
+   private:
+   string fur;
+   public:
+    mammal(string name, int age, bool isHungry,string fur);
+    ~mammal();
+ };
+ 
+ #endif
